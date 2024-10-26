55863fe7ad5877f8776443ea8f6b8f8c31770ef3
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-10-27 01:54:39 +0200

    Ajout du banner

--- frontend/src/home/Banner.jsx
+++ frontend/src/home/Banner.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+
+const title = (
+    <h2>Search Your One From Thousand of Products </h2>
+)
+
+const desc = "We have the largest collection of products"
+
 
 const Banner = () => {
   return (
