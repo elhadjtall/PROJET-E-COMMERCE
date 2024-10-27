0b6ae32f85027d7b7d178d42e01a926af2027864
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-10-27 02:41:32 +0200

    AJout de la barre de recherche

--- frontend/src/home/Banner.jsx
+++ frontend/src/home/Banner.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
+import productData from '../products.json'
 
-const title = <h2>Search Your One From Thousand of Products </h2>
+const title = <h2>Search Your One From <span>Thousand</span>of Products </h2>
 const desc = "We have the largest collection of products"
 const bannerList = [
     {
@@ -18,11 +20,20 @@ const bannerList = [
 ];
 
 const Banner = () => {
+
+    const {searchInput, setSearchInput} = useState('');
+    const {filteredProducts, setFilteredProducts} = useState([]);
+
+    // Après l'importation du fichier json, nous pouvons l'utiliser pour la recherche d'un produit
+    console.log(productData);
   return (
     // banner section permet d'afficher le text de recherche d'un produit au milieu de la page
-    <div className='banner-section style-4'>
+    <div className='banner-section style-4'> 
         <div className='container'>
             <div className='banner-content'>{title}</div>
+                <form>
+                <input type='text' name='search' id='search' placeholder='Serach your product'/>
+                </form>
         </div>
     </div>
   )
