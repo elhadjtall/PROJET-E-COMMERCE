5f82dd7772a0aa53c5cd1a285f4f520d109a77b5
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-10-31 15:50:26 +0100

    Ajout des images category case

--- frontend/src/home/CategoryShowCase.jsx
+++ frontend/src/home/CategoryShowCase.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
+import Ratting from '../components/Ratting';
+import { Link } from 'react-router-dom';
 
 const title = "Our Products";
 
@@ -60,6 +62,24 @@ const productData = [
         price: "50.00",
         id: 6,
     },
+    {
+        imgUrl : "src/assets/images/categoryTab/07.jpg",
+        cate: "Shoes",
+        title: "Nike Premier X",
+        author: "assets/images/courses/author/07.jpg",
+        brand: "Nike",
+        price: "50.00",
+        id: 7,
+    },
+    {
+        imgUrl : "src/assets/images/categoryTab/08.jpg",
+        cate: "Shoes",
+        title: "Nike Premier X",
+        author: "assets/images/courses/author/08.jpg",
+        brand: "Nike",
+        price: "50.00",
+        id: 8,
+    },
 ]
 const CategoryShowCase = () => {
     // selection des product data
@@ -78,11 +98,48 @@ const CategoryShowCase = () => {
                 <div className='course-filter-group'>
                     <ul className='lab-ul'>
                         <li onClick={() => filterItem("All")}>All</li>
-                        <li onClick={() => filterItem("All")}>Shoes</li>
-                        <li onClick={() => filterItem("All")}>Bags</li>
-                        <li onClick={() => filterItem("All")}>Phones</li>
-                        <li onClick={() => filterItem("All")}>Beauty</li>
+                        <li onClick={() => filterItem("Shoes")}>Shoes</li>
+                        <li onClick={() => filterItem("Bags")}>Bags</li>
+                        <li onClick={() => filterItem("Phones")}>Phones</li>
+                        <li onClick={() => filterItem("Beauty")}>Beauty</li>
                     </ul>
+                </div>
+            </div>
+            {/* section bod */}
+            <div className='section-wrapper'>
+                <div className='row g-4 justify-content-center row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1'>
+                    {
+                        items.map((product) => <div key={product.id} className='col'>
+                            <div className='course-item'>
+                                <div className='course-inner'>
+                                    <div className='course-thumb'>
+                                        <img src={product.imgUrl} alt="" />
+                                        <div className='course-category'>
+                                        <div className='course-cate'>
+                                            <a href="#">{product.cate}</a>
+                                        </div>
+                                        <div className='course-review'>
+                                            <Ratting/>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    {/* content */}
+                                    <div className='course-content'>
+                                        <Link to={"/shop/${product.id}"}><h5>{product.title}</h5></Link>
+                                        <div className='course-footer'>
+                                            <div className='course-author'>
+                                            <Link to="/" className='ca-name'>{product.brand}</Link>
+                                            </div>
+                                            <div className='course-price'>
+                                                {product.price}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>)
+                    }
                 </div>
             </div>
         </div>
