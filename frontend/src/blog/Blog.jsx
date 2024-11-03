fb1d2c40defae5e0ed51613c517e14d66749746b
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-11-03 23:20:36 +0100

    AJout de la page shop

--- frontend/src/blog/Blog.jsx
+++ frontend/src/blog/Blog.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageHeader from '../components/PageHeader'
 
 const Blog = () => {
   return (
     <div>
-      Blog
+     <PageHeader title="Blog Page" curPage="Blog"/>
     </div>
   )
 }
--- frontend/src/shop/Shop.jsx
+++ frontend/src/shop/Shop.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import PageHeader from '../components/PageHeader'
+import React, { useState } from 'react';
+import PageHeader from '../components/PageHeader';
 
 const Shop = () => {
+  const [GridList, setGridList] = useState(true);
+  const showResults = "Showing 1–9 of 20 results"; // Déclarez la variable showResults ici
+
   return (
     <div>
-      <PageHeader />
+      <PageHeader title="Our Shop Page" curPage="Shop"/>
+      {/* shop page */}
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            {/* left side le bloc de gauche */}
+            <div className="col-lg-8 col-12">
+             <article>
+              <div className='shop-title d-flex flex-wrap justify-content-between'>
+                <p>{showResults}</p>
+                <div className={`product-view-mode ${GridList ? 'gridActive' : 'listActive'}`}>
+                  <a className='grid' onClick={() => setGridList(!GridList)}>
+                    <i className="icofont-ghost"></i>
+                  </a>
+                  <a className='list' onClick={() => setGridList(!GridList)}>
+                    <i className="icofont-listine-dots"></i>
+                  </a>
+                </div>
+              </div>
+             </article>
+            </div>
+            {/* fin du bloc */}
+            {/* Debut right side le bloc de droite */}
+            <div className="col-lg-4 col-12">
+              right side
+            </div>
+            {/* fin du bloc de droite */}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
