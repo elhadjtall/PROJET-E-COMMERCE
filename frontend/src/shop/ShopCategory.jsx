51d5acd8307569dc9ebab76b037d547877736067
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-11-18 14:44:30 +0100

    Ajustement du code

--- frontend/src/shop/ShopCategory.jsx
+++ frontend/src/shop/ShopCategory.jsx
@@ -7,12 +7,12 @@ const ShopCategory = ({ filterItem, setItem, menuItems, setProducts, selectedCat
         <h5 className='ms-2'>All Categories</h5>
       </div>
       <div>
-        <button className={``}></button>
+        <button className={`ms-2 ${selectedCategory === "All" ? "bg-warning" : ""}`}>All</button>
         {
           menuItems.map((val, id) => {
             return (
               <button
-              className={`m-2 ${selectedCategory === "All" ? "bg-warning" : ""}`}
+              className={`m-2 ${selectedCategory === val ? "bg-warning" : ""}`}
               key={id}
               onClick={() => filterItem(val)}
               >
